10f258d110e0d9ba7b5a607950cdee4a4714609c
Jiane <haroundjihane0@gmail.com>
2026-02-14 13:17:56 +0100

    add global footer and Bootstrap JS

--- index.php
+++ index.php
@@ -235,6 +235,8 @@ if (isset($_GET['page']) && $_GET['page'] == 9) {
     }
     ?>
 </main>
+<?php include("vue/public/footer.php"); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
